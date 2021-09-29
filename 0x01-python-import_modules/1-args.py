c38356894561bd7e3ee4cecad5be7530867e7781
Hope-Parnell <parnell.hope.c@gmail.com>
2021-09-29 13:56:19 +0000

    Add python file

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    if argc == 1:
+        print("{} arguments.".format(argc - 1))
+    else:
+        print("{} arguments:".format(argc - 1))
+        for x in range(1, argc):
+            print("{}: {}".format(x, sys.argv[x]))
