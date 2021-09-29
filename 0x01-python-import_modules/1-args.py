f556ec40744978efdc2e23284c9e1ac8893b946c
Hope-Parnell <parnell.hope.c@gmail.com>
2021-09-29 14:48:06 +0000

    fixed output

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -2,9 +2,8 @@
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
-    if argc == 1:
-        print("{} arguments.".format(argc - 1))
-    else:
-        print("{} arguments:".format(argc - 1))
+    print("{} {}{}".format(argc - 1, "argument" if argc == 2 else "arguments",
+                           "." if argc == 1 else ":"))
+    if argc > 1:
         for x in range(1, argc):
             print("{}: {}".format(x, sys.argv[x]))
