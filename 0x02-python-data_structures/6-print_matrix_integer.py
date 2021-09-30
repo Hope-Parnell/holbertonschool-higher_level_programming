f3dd11cf98ebcf89ecdea455f55434eeb37d6997
Hope-Parnell <parnell.hope.c@gmail.com>
2021-09-30 17:24:54 +0000

    changed to printing ints

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         print("")
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-            print("{}".format(matrix[x][y]),
+            print("{:d}".format(matrix[x][y]),
                   end=" " if y < len(matrix[x]) - 1 else "\n")
