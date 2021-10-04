073047f3682a32ca4d5d4402ca151dea4fea8780
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-04 17:11:13 +0000

    changed for loop to while loop for better iteration

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@ def roman_to_int(roman_string):
         return 0
     rtoi = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     total = 0
-    for i in range(len(roman_string)):
+    i = 0
+    while i < len(roman_string):
         a = roman_string[i]
         if (i + 1) != len(roman_string):
             b = roman_string[i + 1]
@@ -12,7 +13,9 @@ def roman_to_int(roman_string):
             b = None
         if b is not None and rtoi[a] < rtoi[b]:
             i += 1
-            total += rtoi[b] - rtoi[a]
+            num = rtoi[b] - rtoi[a]
+            total += num
         else:
             total += rtoi[a]
+        i += 1
     return total
