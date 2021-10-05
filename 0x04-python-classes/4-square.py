ea584fb27129de4dbf0a729f3372a5d01b6b8e50
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-05 21:44:47 +0000

    add Python file(s)

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This module creates a class with the private attribute size
+and checks for valid size input and defines an area function
+as well as ways to retrieve and change size
+"""
+
+
+class Square:
+    """create a class Square and use __init__ to set size"""
+    def __init__(self, size=0):
+        '''initilizes the square after checking for valid input
+
+        Args:
+            size (int): the size of the square
+        '''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''sets the size of the square
+
+        Args:
+            value (int): the size of the square
+        '''
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints the square"""
+        if self.size == 0:
+            print()
+        for x in range(self.size):
+            for y in range(self.size):
+                print("#", end="")
+            print()
