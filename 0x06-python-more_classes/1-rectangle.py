c21aed5d773e7bbe734afc4a2fa463980772e40d
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-08 16:22:44 +0000

    made perimeter 0 if height or width are 0

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -65,4 +65,6 @@ class Rectangle:
 
     def perimeter(self):
         """returns the perimeter of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
         return (2 * self.__width) + (2 * self.__height)
