abad79be19146584d4a0a15633499cd568855700
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-12 00:18:28 +0000

    task 3 done

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This Module prints a name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function to print a person's name
+
+    Args:
+        first_name(string): the person's first name
+        last_name(string): the person's last name, defaults to empty
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Module includes print_square function for 
+learning to make test files
+"""
+
+
+def print_square(size):
+    """prints a square of size
+    
+    Args:
+        size(int): length of the sides of the square
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    
+    for x in range(size):
+        print("#" * size)
