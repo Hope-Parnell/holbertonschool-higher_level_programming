cb1a24eb1beaca87ca693ad079cb395ae7fc6c96
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-12 01:07:50 +0000

    finished taskn 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""This module separates a string"""
+
+
+def text_indentation(text):
+    """This function splits a string
+
+    Arg:
+        text(string): the string to be split
+    """
+    separate = False
+    split = True
+    separators = [".", ":", "?"]
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for c in text:
+        if separate is True:
+            print("\n")
+            separate = False
+            split = True
+        if c is " " and split is True:
+            continue
+        else:
+            if c is not "\n":
+                split = False
+            print("{}".format(c), end="")
+        if c in separators:
+            separate = True
