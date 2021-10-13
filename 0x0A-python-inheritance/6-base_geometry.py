69d81e6940f9c697288b1c1e09d2719830ff526f
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-13 19:00:18 +0000

    add Python file(s)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module contains an class with an unimplemented function"""
+
+
+class BaseGeometry:
+    """Class with unimplimented function"""
+    def area(self):
+        """Function raises an exception"""
+        raise Exception("area() is not implemented")
+
