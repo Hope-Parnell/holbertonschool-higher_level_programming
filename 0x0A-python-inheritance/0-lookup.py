86630af3f29950805efe28372e3076e58044621d
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-13 17:47:16 +0000

    add Python function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module gets object properties"""
+
+
+def lookup(obj):
+    """Function returns the list of available attributes and 
+    methods of an object
+    
+    Arg:
+        obj(object): the object to be searched
+    """
+    return dir(obj)
