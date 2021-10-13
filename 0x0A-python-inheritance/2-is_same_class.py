32912cf32d20331b612dd205ea5c0eb8ee92fd52
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-13 18:34:07 +0000

    add Python function

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module checks the class of an object"""
+
+
+def is_same_class(obj, a_class):
+    """Function checks if the object is exactly an 
+    instance of the specified class
+    
+    Args:
+        obj(object): the object to check
+        a_class(class): the class to check against
+    """
+    return True if type(obj) is a_class else False
