b8f49f82c873f4a99fcd34f3d813a1072c98760d
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-15 15:34:42 +0000

    add Python function

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module opens and reads from a file"""
+
+
+def read_file(filename=""):
+    """Prints file content to standard output
+
+    Arg:
+        filename(str): the file to print
+    """
+    with open(filename) as f:
+        print(f.read(), end = "")
