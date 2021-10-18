b02376782d6b2354983c2834ce5a883f1cbde782
Hope-Parnell <parnell.hope.c@gmail.com>
2021-10-18 16:52:03 +0000

    Added value verifiers

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,10 +13,30 @@ class Rectangle(Base):
         Args:
             width(int): width of the rectangle
             height(int): height of the rectangle
-            x: graphical x coordinate of the rectangle
-            y: graphical y coordinate of the rectangle
+            x: vertical location of the rectangle
+            y: horizontal location of the rectangle
             id: the id for the rectangle
         """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
+        if type(y) is not int:
+            raise TypeError("x must be an integer")
+        if y < 0:
+            raise ValueError("x must be >= 0")
+
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -58,10 +78,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """sets the x coordinate of the rectangle
+        """sets the x of the rectangle
 
         Arg:
-            value: new x coordinate for the rectangle
+            value: new x for the rectangle
         """
         self.__x = value
 
@@ -72,10 +92,10 @@ class Rectangle(Base):
 
     @x.setter
     def y(self, value):
-        """sets the y coordinate of the rectangle
+        """sets the y of the rectangle
 
         Arg:
-            value: new y coordinate for the rectangle
+            value: new y for the rectangle
         """
         self.__y = value
 
