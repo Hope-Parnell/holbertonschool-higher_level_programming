eee4c7906296e8975fda8706bfe65e033d8557e5
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-03 14:46:22 -0500

    task 12

---
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lists shows with no genre
+-- genre id will be 'NULL'
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
