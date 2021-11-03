53554c3463e154b9382193dc08fc33821ef03baa
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-03 14:49:36 -0500

    task 13

---
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- Lists how many shows each genre has
+-- Omits genres without shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_show_genres LEFT JOIN tv_genres ON tv_genres.id = genre_id
+GROUP BY genre_id ORDER BY number_of_shows DESC;
