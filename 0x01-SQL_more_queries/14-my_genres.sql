16727871c95a475739a980244ccc09fdfc5fee74
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-03 15:54:31 -0500

    task 14

---
+++ 0x01-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- List genres for 'Dexter'
+-- Dexter could be any id
+SELECT tv_genres.name
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = "Dexter"
+
