f00a59360f9ab671a976706800285a333e4d0984
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-03 12:36:48 -0500

    task 0

--- 0x01-SQL_more_queries/0-privileges.sql
+++ 0x01-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,4 @@
 -- shows privlages for users 'user_0d_1' and 'user_0d_2'
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost'
+-- more comment stff
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost'
