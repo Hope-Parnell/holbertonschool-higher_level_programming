73746df6764c6048418ba27c43b8c0242c2f74fe
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-15 15:51:38 -0600

    finished task 7

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """the State class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
