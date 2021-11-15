8c3a8c3c3645e7109cc0f1b14af0c62ca7d2344f
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-15 14:51:14 -0600

    finished task 3

---
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module selects states  starting with 'N' from a database"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE BINARY name = %s \
+                ORDER BY id ASC", (argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
