357f5aff663ef175f90c64a879f4f89dafc7fdf2
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-15 14:06:37 -0600

    fixed spacing for pycodestyle

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -3,6 +3,7 @@
 from sys import argv
 import MySQLdb
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
