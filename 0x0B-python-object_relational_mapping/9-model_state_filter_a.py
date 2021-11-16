039818665297665903a4701799de92f7414a6e22
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-16 12:25:48 -0600

    pycodestyling

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,5 +13,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(State.name.ilike('%a%')).order_by(State.id):
+    for state in session.query(State).filter(
+            State.name.ilike('%a%')).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
