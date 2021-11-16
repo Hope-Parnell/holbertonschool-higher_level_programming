5cc94e7cdab5e781372bc36dec7cb15d23f74f9a
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-16 13:13:32 -0600

    changed import

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Module selects states from a database"""
-from sys import argv
+import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
