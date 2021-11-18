e35e28300ac50ae97aa77e789278dc4bda1b97e9
Hope-Parnell <parnell.hope.c@gmail.com>
2021-11-18 12:19:06 -0600

    a

--- 0x0B-python-object_relational_mapping/relationship_state.py
+++ 0x0B-python-object_relational_mapping/relationship_state.py
@@ -3,7 +3,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
+from relationship_city import City
 
 Base = declarative_base()
 
@@ -13,4 +13,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")
