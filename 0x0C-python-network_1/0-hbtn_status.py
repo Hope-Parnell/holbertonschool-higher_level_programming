b6cd7716f0aa395772588b5170c27ff4e52b4c24
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-01 13:00:05 -0600

    bleh

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body Response: ")
+        print("Body Response:")
         print(" - type: {}".format(type(html)))
         print(" - content: {}".format(str(html)))
         print(" - utf8 content: {}".format(html.decode('UTF-8')))
