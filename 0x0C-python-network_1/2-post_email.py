1834ad041d24b3d285630047fde7d9f1eb74e621
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-01 13:10:45 -0600

    forgot 'if __name__ == "__main__":'

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -3,10 +3,12 @@ import urllib.parse
 import urllib.request
 import sys
 
-url = sys.argv[1]
-email = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(email)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-   print(response.read().decode('UTF-8'))
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('UTF-8'))
