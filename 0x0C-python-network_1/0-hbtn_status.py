e42aba7ed70a654e7897a433e2853343ad6bd5d3
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-01 13:04:56 -0600

    trying to get it done

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """fetches status from holberton intranet"""
-import urllib.request
 
+if __name__ == '__main__':
+    import urllib.request
 
-if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body Response:")
-        print(" - type: {}".format(type(html)))
-        print(" - content: {}".format(html))
-        print(" - utf8 content: {}".format(html.decode('UTF-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(str(html)))
+        HTMLdecode = html.decode('UTF-8')
+        print('\t- utf8 content: {}'.format(HTMLdecode))
