b7a4f1033cfe38ca09da8dc75a65c55a9ab07928
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-01 12:42:56 -0600

    decode before print

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -9,4 +9,5 @@ if __name__ == "__main__":
         print("Body Response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(str(html)))
-        print("\t- utf8 content: {}".format(html.decode('UTF-8')))
+        content = html.decode('UTF-8')
+        print("\t- utf8 content: {}".format(content))
