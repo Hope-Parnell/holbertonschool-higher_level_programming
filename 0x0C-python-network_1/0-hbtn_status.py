c196dd36c1c723fc15ffd3194eea8bed73f48349
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-01 12:56:36 -0600

    left out a space in first print...

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,10 +4,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body Response:")
+        print("Body Response: ")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(str(html)))
         print("\t- utf8 content: {}".format(html.decode('UTF-8')))
