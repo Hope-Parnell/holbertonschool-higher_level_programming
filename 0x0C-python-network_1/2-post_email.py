a504f5d5d71cfaadda046e6249a1619c30f5ae1e
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-01 13:13:29 -0600

    changed around variables

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -6,8 +6,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(email)
+    email = sys.argv[2]
+    val = {'email': email}
+    data = urllib.parse.urlencode(val)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
