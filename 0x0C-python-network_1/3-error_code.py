7c3adc8edbaf1d2db4f8afc2dbf492797a94e9e9
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-01 13:25:01 -0600

    task 3

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""catches errors from requests"""
+import urllib.request
+from urllib.error import HTTPError
+import sys
+
+
+if __name__ == "__main__":
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print("Error code: {}".format(e))
