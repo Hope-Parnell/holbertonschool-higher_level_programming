c3316bd4e592f4c2ea7ccbeb2ec6e05f8edc23a1
Hope-Parnell <parnell.hope.c@gmail.com>
2022-01-07 10:59:01 -0600

    task 3

---
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    w = Number(w);
+    h = Number(h);
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
