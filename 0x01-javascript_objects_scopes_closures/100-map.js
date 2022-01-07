59e98f26e28094b1bcc1f44bf62620f069edf7e6
Hope-Parnell <parnell.hope.c@gmail.com>
2022-01-07 12:17:03 -0600

    changed syntax

--- 0x01-javascript_objects_scopes_closures/100-map.js
+++ 0x01-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-const newList = list.map(x => x * list.indexOf(x));
+const newList = list.map((number, index) => number * index);
 console.log(newList);
